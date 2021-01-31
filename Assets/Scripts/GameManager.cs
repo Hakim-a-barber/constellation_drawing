1917b856772bfd51d4c958afc3d1d1a166dce275
sophierichards <richas3@rpi.edu>
2021-01-31 17:56:13 -0500

    last changes hopefully

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,7 +28,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            Debug.Log("Close game");
+            Application.Quit();
+        }
     }
 
     public void LoadNextScene(){
